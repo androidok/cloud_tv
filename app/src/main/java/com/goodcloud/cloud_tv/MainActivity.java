a4da7d3345ae3a19f71286c2877d50d7146d8ab5
binglin881211 <binglin881211@163.com>
2019-02-12 20:53:59 +0800

    注释测试

--- app/src/main/java/com/goodcloud/cloud_tv/MainActivity.java
+++ app/src/main/java/com/goodcloud/cloud_tv/MainActivity.java
@@ -3,6 +3,11 @@ package com.goodcloud.cloud_tv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 好用云智慧工厂系统移动端（安卓APP）液晶屏端APP
+
+
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
